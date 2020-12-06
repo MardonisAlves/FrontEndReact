4bcf5ff8e03e2f42196809d357bfbe1171387641
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-12-05 21:04:04 -0300

    first commit

---
+++ src/main/app.jsx
@@ -0,0 +1,11 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/fontawesome.min.css'
+
+import React from 'react'
+
+export default props => (
+    <div className="container">
+            <h1>Teste</h1>
+            
+    </div>
+)
