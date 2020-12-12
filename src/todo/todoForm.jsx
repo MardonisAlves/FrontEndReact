0f1bfc1439872b121bef3462e48302fb1f792aa3
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2020-12-11 21:06:49 -0300

    add-form-button

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+
 export default props => (
-    <div>
-        <h1>Form</h1>
+
+    <div role='form' className="todo-form">
+
+        <Grid cols='12 9 10'>
+            <input id="descripction" className="form-control" placeholder="add tarefa"></input> 
+       </Grid>
+
+
+       <Grid cols='12 3 2'>
+            <IconButton  style="primary" icon="plus"></IconButton>
+       </Grid>
     </div>
 )
